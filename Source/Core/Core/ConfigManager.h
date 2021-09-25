b252fa2f5c746ff0732273dcf64a578517d5fe87
Robert Peralta <rapito@gmail.com>
2021-09-24 22:41:11 -0400

    Enable Custom Matchmaking Server by default

--- Source/Core/Core/ConfigManager.h
+++ Source/Core/Core/ConfigManager.h
@@ -163,7 +163,7 @@ struct SConfig : NonCopyable
 	bool m_slippiForceNetplayPort = false;
 	int m_slippiNetplayPort;
 	bool m_slippiForceLanIp = false;
-	bool m_slippiCustomMMEnabled = false;
+	bool m_slippiCustomMMEnabled = true;
 	std::string m_slippiCustomMMServerURL = "165.227.213.180";
 	std::string m_slippiCustomMMReportingURL = "https://lylat.gg/reports";
 	std::string m_slippiLanIp = "";
