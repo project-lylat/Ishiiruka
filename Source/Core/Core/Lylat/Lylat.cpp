07b96ef58e4ad08a155f0443bf926507398ef0e0
Robert Peralta <rapito@gmail.com>
2023-08-17 22:53:09 -0400

    Add Lylat cpp and headers to windows solution

--- Source/Core/Core/Lylat/Lylat.cpp
+++ Source/Core/Core/Lylat/Lylat.cpp
@@ -1,2 +1,40 @@
 #include "Lylat.h"
 
+
+
+const std::string Lylat::SLIPPI_HOSTNAME = "https://slippi.gg";
+const std::string Lylat::SLIPPI_USER_JSON_URL = "https://users-rest-dot-slippi.uc.r.appspot.com/user";
+const std::string Lylat::SLIPPI_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/report";
+const std::string Lylat::SLIPPI_ABANDON_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/abandon";
+const std::string Lylat::SLIPPI_COMPLETE_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/complete";
+const std::string Lylat::SLIPPI_USER_LOGIN_URL = SLIPPI_HOSTNAME + "/online/enable";
+const std::string Lylat::SLIPPI_MATCHMAKING_URL = "mm.slippi.gg";
+const std::string Lylat::SLIPPI_DEV_MATCHMAKING_URL = "mm2.slippi.gg";
+const std::string Lylat::LYLAT_STAGING_MATCHMAKING_URL = "159.203.86.6";
+
+#ifdef LYLAT_STAGING
+const std::string Lylat::HOSTNAME = "http://159.203.86.6";
+const std::string Lylat::MATCHMAKING_URL = HOSTNAME;
+#elif defined(LYLAT_DEV)
+const std::string Lylat::HOSTNAME = "http://localhost:3000";
+const std::string Lylat::MATCHMAKING_URL = "http://localhost";
+#elif defined(LYLAT_SLIPPI)
+const std::string Lylat::HOSTNAME = "https://slippi.gg";
+const std::string Lylat::MATCHMAKING_URL = HOSTNAME;
+#else
+const std::string Lylat::HOSTNAME = "https://lylat.gg";
+const std::string Lylat::MATCHMAKING_URL = HOSTNAME;
+#endif
+
+#if defined(LYLAT_SLIPPI)
+const std::string Lylat::USER_LOGIN_URL = SLIPPI_USER_LOGIN_URL;
+const std::string Lylat::GAME_REPORT_URL = SLIPPI_GAME_REPORT_URL;
+const std::string Lylat::USER_JSON_URL = SLIPPI_USER_JSON_URL;
+#else
+const std::string Lylat::USER_LOGIN_URL = HOSTNAME + "/users/enable";
+const std::string Lylat::GAME_REPORT_URL = HOSTNAME + "/reports";
+const std::string Lylat::ABANDON_GAME_REPORT_URL = HOSTNAME + "/reports/abandon";   // TODO: create on server
+const std::string Lylat::COMPLETE_GAME_REPORT_URL = HOSTNAME + "/reports/complete"; // TODO: create on server
+const std::string Lylat::USER_JSON_URL = HOSTNAME + "/users/slp";
+const std::string Lylat::UPDATE_URL = HOSTNAME + "/downloads/melee";
+#endif
--- Source/Core/Core/Lylat/Lylat.h
+++ Source/Core/Core/Lylat/Lylat.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <string>
 
-//#define LYLAT_ENV_STAGING "STAGING"
-//#define LYLAT_ENV_SLIPPI "SLIPPI"
-//#define LYLAT_ENV_DEV "DEV"
+// #define LYLAT_ENV_STAGING "STAGING"
+// #define LYLAT_ENV_SLIPPI "SLIPPI"
+// #define LYLAT_ENV_DEV "DEV"
 #define LYLAT_ENV_PRODUCTION "PRODUCTION"
 
 #ifdef LYLAT_ENV_STAGING
@@ -18,40 +19,23 @@
 class Lylat
 {
   public:
-	static const inline std::string SLIPPI_HOSTNAME = "https://slippi.gg";
-	static const inline std::string SLIPPI_USER_JSON_URL = "https://users-rest-dot-slippi.uc.r.appspot.com/user";
-	static const inline std::string SLIPPI_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/report";
-	static const inline std::string SLIPPI_ABANDON_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/abandon";
-	static const inline std::string SLIPPI_COMPLETE_GAME_REPORT_URL = "https://rankings-dot-slippi.uc.r.appspot.com/complete";
-	static const inline std::string SLIPPI_USER_LOGIN_URL = SLIPPI_HOSTNAME + "/online/enable";
-	static const inline std::string SLIPPI_MATCHMAKING_URL = "mm.slippi.gg";
-	static const inline std::string SLIPPI_DEV_MATCHMAKING_URL = "mm2.slippi.gg";
-	static const inline std::string LYLAT_STAGING_MATCHMAKING_URL = "159.203.86.6";
+	static const std::string SLIPPI_HOSTNAME;
+	static const std::string SLIPPI_USER_JSON_URL;
+	static const std::string SLIPPI_GAME_REPORT_URL;
+	static const std::string SLIPPI_ABANDON_GAME_REPORT_URL;
+	static const std::string SLIPPI_COMPLETE_GAME_REPORT_URL;
+	static const std::string SLIPPI_USER_LOGIN_URL;
+	static const std::string SLIPPI_MATCHMAKING_URL;
+	static const std::string SLIPPI_DEV_MATCHMAKING_URL;
+	static const std::string LYLAT_STAGING_MATCHMAKING_URL;
 
-#ifdef LYLAT_STAGING
-	static const inline std::string HOSTNAME = "http://159.203.86.6";
-	static const inline std::string MATCHMAKING_URL = HOSTNAME;
-#elif defined(LYLAT_DEV)
-	static const inline std::string HOSTNAME = "http://localhost:3000";
-	static const inline std::string MATCHMAKING_URL = "http://localhost";
-#elif defined(LYLAT_SLIPPI)
-	static const inline std::string HOSTNAME = "https://slippi.gg";
-	static const inline std::string MATCHMAKING_URL = HOSTNAME;
-#else
-	static const inline std::string HOSTNAME = "https://lylat.gg";
-	static const inline std::string MATCHMAKING_URL = HOSTNAME;
-#endif
+	static const std::string HOSTNAME;
+	static const std::string MATCHMAKING_URL;
 
-#if defined(LYLAT_SLIPPI)
-	static const inline std::string USER_LOGIN_URL = SLIPPI_USER_LOGIN_URL;
-	static const inline std::string GAME_REPORT_URL = SLIPPI_GAME_REPORT_URL;
-	static const inline std::string USER_JSON_URL = SLIPPI_USER_JSON_URL;
-#else
-	static const inline std::string USER_LOGIN_URL = HOSTNAME + "/users/enable";
-	static const inline std::string GAME_REPORT_URL = HOSTNAME + "/reports";
-	static const inline std::string ABANDON_GAME_REPORT_URL = HOSTNAME + "/reports/abandon"; // TODO: create on server
-	static const inline std::string COMPLETE_GAME_REPORT_URL = HOSTNAME + "/reports/complete"; // TODO: create on server
-	static const inline std::string USER_JSON_URL = HOSTNAME + "/users/slp";
-	static const inline std::string UPDATE_URL = HOSTNAME + "/downloads/melee";
-#endif
+	static const std::string USER_LOGIN_URL;
+	static const std::string GAME_REPORT_URL;
+	static const std::string ABANDON_GAME_REPORT_URL;
+	static const std::string COMPLETE_GAME_REPORT_URL;
+	static const std::string USER_JSON_URL;
+	static const std::string UPDATE_URL;
 };
